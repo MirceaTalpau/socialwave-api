87fa3d2599e390ede65833a224824f49fdb2e2ed
Tălpău Mircea Cristian <mircea.t@admintools.io>
2024-12-05 10:57:46 +0200

    updated

--- src/modules/follow/follow.controller.ts
+++ src/modules/follow/follow.controller.ts
@@ -12,37 +12,37 @@ export class FollowController {
     return await this.followService.getFollowRequests(user);
   }
   @Post('request')
-  async requestFollow(@Req() req, @Body() userId: number) {
+  async requestFollow(@Req() req, @Body() folowee: { userId: number }) {
     const user = req.user;
     const followRequest = new RequestFollowDto();
     followRequest.followerId = user;
-    followRequest.followeeId = userId;
+    followRequest.followeeId = folowee.userId;
     return await this.followService.requestFollow(followRequest);
   }
 
   @Post('accept')
-  async acceptFollow(@Req() req, @Body() userId: number) {
+  async acceptFollow(@Req() req, @Body() folowee: { userId: number }) {
     const followRequest = new RequestFollowDto();
     const user = req.user;
-    followRequest.followerId = userId;
+    followRequest.followerId = folowee.userId;
     followRequest.followeeId = user;
     return await this.followService.acceptFollow(followRequest);
   }
 
   @Post('reject')
-  async rejectFollow(@Req() req, @Body() userId: number) {
+  async rejectFollow(@Req() req, @Body() folowee: { userId: number }) {
     const followRequest = new RequestFollowDto();
     const user = req.user;
-    followRequest.followerId = userId;
+    followRequest.followerId = folowee.userId;
     followRequest.followeeId = user;
     return await this.followService.rejectFollow(followRequest);
   }
   @Post('unfollow')
-  async unfollow(@Req() req, @Body() userId: number) {
+  async unfollow(@Req() req, @Body() folowee: { userId: number }) {
     const followRequest = new RequestFollowDto();
     const user = req.user;
     followRequest.followerId = user;
-    followRequest.followeeId = userId;
+    followRequest.followeeId = folowee.userId;
     return await this.followService.unfollow(followRequest);
   }
 }
