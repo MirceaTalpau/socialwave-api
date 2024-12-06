948c2f48bd46b52434f3e83ebb991c68df76ab00
Tălpău Mircea Cristian <mircea.t@admintools.io>
2024-12-06 10:13:07 +0200

    added feed

---
+++ src/modules/feed/dtos/FeedResponse.dto.ts
@@ -0,0 +1,9 @@
+export class FeedResponseDto {
+  userId: number;
+  name: string;
+  profilePicture: string;
+  createdAt: Date;
+  description: string;
+  images: string[];
+  videos: string[];
+}
--- src/modules/feed/feed.service.ts
+++ src/modules/feed/feed.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import 'dotenv/config';
-import { followRequestsTable, postsTable } from 'src/db/schema';
-import { eq } from 'drizzle-orm';
+import {
+  followRequestsTable,
+  imagesPostTable,
+  postsTable,
+  usersTable,
+  videosPostTable,
+} from 'src/db/schema';
+import { and, desc, eq } from 'drizzle-orm';
+import { FeedResponseDto } from './dtos/FeedResponse.dto';
 
 @Injectable()
 export class FeedService {
@@ -12,15 +19,52 @@ export class FeedService {
   }
 
   async getFeed(userId: number) {
-    const feed = await this.db
-      .select()
+    const posts = await this.db
+      .select({
+        userId: usersTable.userId,
+        name: usersTable.name,
+        profilePicture: usersTable.profilePicture,
+        createdAt: postsTable.createdAt,
+        description: postsTable.description,
+        postId: postsTable.postId,
+      })
       .from(postsTable)
       .innerJoin(
         followRequestsTable,
         eq(postsTable.userId, followRequestsTable.followeeId),
       )
-      .where(eq(followRequestsTable.followerId, userId))
+      .innerJoin(usersTable, eq(postsTable.userId, usersTable.userId))
+
+      .where(
+        and(eq(followRequestsTable.followerId, userId)),
+        eq(followRequestsTable.isAccepted, true),
+      )
       .orderBy(postsTable.createdAt, 'desc');
+    const feed: FeedResponseDto[] = [];
+    for (const post of posts) {
+      const images = await this.db
+        .select({
+          images: imagesPostTable.imageUrl,
+        })
+        .from(imagesPostTable)
+        .where(eq(imagesPostTable.postId, post.postId));
+      const videos = await this.db
+        .select({
+          videos: videosPostTable.videoUrl,
+        })
+        .from(videosPostTable)
+        .where(eq(videosPostTable.postId, post.postId));
+      feed.push({
+        userId: post.userId,
+        name: post.name,
+        profilePicture: post.profilePicture,
+        createdAt: post.createdAt,
+        description: post.description,
+        images: images.map((image) => image.images),
+        videos: videos.map((video) => video.videos),
+      });
+    }
+
     return feed;
   }
 }
